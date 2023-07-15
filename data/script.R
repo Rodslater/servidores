95142806c64315d347ad2d85f66ffca00d115164
Rodslater <91638825+Rodslater@users.noreply.github.com>
2023-07-15 14:31:40 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -1,25 +1,82 @@
-library(dplyr)
-library(downloader)
+library(httr)
 library(lubridate)
+library(downloader)
+library(dplyr)
 
-mes <- month(Sys.Date())-2
-mes <- ifelse(mes<10, paste0('0',mes), mes)
+datas <- c()
+for (i in 0:5) {
+  mes <- month(Sys.Date()) - i
+  mes <- ifelse(mes < 10, paste0('0', mes), mes)
+  
+  anomes <- paste0(year(Sys.Date()), mes)
+  
+  datas <- c(datas, anomes)
+}
 
-anomes <- paste0(year(Sys.Date()), mes)
 
-options(timeout=1000)
-url <- paste0('https://portaldatransparencia.gov.br/download-de-dados/servidores/', anomes, '_Servidores_SIAPE')
-download(url, dest="dataset.zip", mode="wb") 
-unzip ("dataset.zip")
+# Variável de controle para verificar se o primeiro arquivo foi baixado com sucesso
+baixado <- FALSE
 
-cadastros <- paste0(anomes, '_Cadastro.csv')
-afastamentos <- paste0(anomes, '_Afastamentos.csv')
-observacoes <- paste0(anomes, '_Observacoes.csv')
-remuneracao <- paste0(anomes, '_Remuneracao.csv')
+# Loop de baixar as séries
+for (i in seq_along(datas)) {
+  arquivo <- sprintf("dataset_%s.zip", datas[i])
+  
+  # Verifica se o arquivo já existe
+  if (file.exists(arquivo)) {
+    message(paste("Arquivo", arquivo, "já foi baixado. Continuando com o próximo arquivo."))
+    next
+  }
+  
+  url <- paste0('https://portaldatransparencia.gov.br/download-de-dados/servidores/', datas[i], '_Servidores_SIAPE', '.zip')
+  
+  # Verifica se o arquivo existe antes de fazer o download
+  response <- tryCatch(
+    {
+      GET(url)
+    },
+    error = function(e) {
+      return(NULL)
+    }
+  )
+  
+  # Se a resposta é NULL, o arquivo não existe, então passa para a próxima iteração
+  if (is.null(response)) {
+    message(paste("Arquivo não encontrado:", arquivo))
+    return()
+  }
+  
+  # Verifica se o primeiro arquivo já foi baixado com sucesso
+  if (!baixado) {
+    tryCatch(
+      {
+        download(url, dest = arquivo, mode = "wb")
+        unzip(arquivo)
+        file.remove(arquivo)
+        arquivos_csv <- list.files(pattern = "\\.csv$", full.names = TRUE)
+        padroes_mantidos <- c(".*Cadastro\\.csv$")
+        arquivos_remover <- arquivos_csv[!grepl(paste(padroes_mantidos, collapse = "|"), arquivos_csv)]
+        file.remove(arquivos_remover)
+        
+        # Define a variável baixado como TRUE
+        baixado <- TRUE
+        
+        # Importar o arquivo CSV final
+        arquivo_final <- arquivos_csv[grepl(paste(padroes_mantidos, collapse = "|"), arquivos_csv)]
+        cadastro <- read.csv2(arquivo_final, dec =",", fileEncoding='latin1')
+        
+        # Remover o arquivo CSV final
+        file.remove(arquivo_final)
+        
+      },
+      error = function(e) {
+        message(paste("Erro ao baixar o arquivo:", arquivo))
+        return()
+      }
+    )
+  }
+}
 
-cadastro <- read.csv2(cadastros, dec =",", fileEncoding='latin1')
-
-file.remove(c('dataset.zip', cadastros, afastamentos, observacoes, remuneracao))
+ 
 
 
 atrasados <- c(102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,
@@ -28,18 +85,14 @@ atrasados <- c(102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,
 
 vetor_tecnicos <- c(101:116, 201:216, 301:316, 401:416)
 
-tecnicosBR <- cadastro %>% 
-  filter(grepl("Instituto Federal", ORG_LOTACAO), DESCRICAO_CARGO!= "Sem informação", DESCRICAO_CARGO!= "Inválido", !grepl("PROF", DESCRICAO_CARGO)) %>% 
-   filter(PADRAO_CARGO%in%vetor_tecnicos) %>%
-  select(ORG_LOTACAO, CPF, NOME, DESCRICAO_CARGO, CLASSE_CARGO, PADRAO_CARGO) %>% 
-  mutate(PADRAO_CARGO = as.numeric(PADRAO_CARGO)) %>% 
+tecnicosBR <- cadastro |> 
+  filter(grepl("Instituto Federal", ORG_LOTACAO), DESCRICAO_CARGO!= "Sem informação", DESCRICAO_CARGO!= "Inválido", !grepl("PROF", DESCRICAO_CARGO)) |> 
+  filter(PADRAO_CARGO%in%vetor_tecnicos) |>
+  select(ORG_LOTACAO, CPF, NOME, DESCRICAO_CARGO, CLASSE_CARGO, PADRAO_CARGO) |> 
+  mutate(PADRAO_CARGO = as.numeric(PADRAO_CARGO)) |> 
   arrange(ORG_LOTACAO, PADRAO_CARGO, NOME)
 
 names(tecnicosBR) <- c("Orgao", "CPF","Nome","Cargo", "Classe", "Padrao")
 
-atrasadosBR <- tecnicosBR %>% 
-  filter(Padrao%in%atrasados)
-  
 
 saveRDS(tecnicosBR,'data/tecnicosBR.rds')
-
